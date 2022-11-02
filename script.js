b4592df3dc62b416e7548b763691440d05961a1f
Ben Pollard <benpollard26@gmail.com>
2022-11-02 17:22:52 +0000

    Modified grid to only redraw cells that have been modified and move cells to 2d array

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.style.width = '100%';
 canvas.style.height = '100%';
 canvas.width  = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
+
+let mousePressed = false
 
 console.log(canvas.getBoundingClientRect())
 
@@ -12,42 +16,59 @@ window.addEventListener('resize', function() {
 })
 
 canvas.addEventListener('mousemove', function(e) {
-    console.log(e)
     draw(e.clientX, e.clientY);
 })
 
-let rects = [];
+canvas.addEventListener('mousedown', function(e) {
+    mousePressed = true
+})
 
-const h = 2;
-const w = 2;
+canvas.addEventListener('mouseup', function(e) {
+    mousePressed = false
+})
+
+let pixels = [];
+
+const h = 20;
+const w = 20;
 const ps = canvas.height / h
+
+let prevPixel = {x: undefined, y: undefined}
+
 
 
 function generateGrid() {
-    // ctx.scale(5,5)
-    console.log(ps)
     for(let i = 0; i < h; i++) {
+        pixels[i] = []
         for(let j = 0; j < w; j++) {
-            let p = {x: i * 250 , y: j * 250, fill:  i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0 ? 'white': '#BEBEBE'}
-            rects.push(p)
-            console.log(p)
+            p = {x: i * ps, y: j * ps , fill: i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0 ? 'white': '#D0D0D0'}
+            pixels[i][j] = p
             ctx.fillStyle = p.fill
             ctx.fillRect(p.x, p.y, ps, ps)
         }
     }
 }
 
+function getPixel(x,y) {
+    px = Math.floor(x / ps)
+    py = Math.floor(y / ps)
+    return {x: px, y: py}
+}
+
 function draw(x,y) {
-    let color = 'green'
-    let i = 0
-    while(r = rects[i++]) {
-        ctx.beginPath();
-        ctx.rect(r.x, r.y, ps, ps);    
-        
-        ctx.fillStyle = ctx.isPointInPath(x, y) ? "red" : r.fill;
-        ctx.fill();
-      }
-    
+    p = getPixel(x,y)
+    if (prevPixel.x != p.x && prevPixel.x != undefined || prevPixel.y != p.y && prevPixel.y != undefined) {
+        console.log("fixing prev pixel: " + prevPixel)
+        ctx.fillStyle = pixels[prevPixel.x][prevPixel.y].fill
+        ctx.fillRect(pixels[prevPixel.x][prevPixel.y].x, pixels[prevPixel.x][prevPixel.y].y, ps, ps)
+    } if (prevPixel.x === p.x && prevPixel.y === p.y) {
+        return
+    } else {       
+        prevPixel = p
+        let color = '#888888'
+        ctx.fillStyle = color
+        ctx.fillRect(pixels[p.x][p.y].x, pixels[p.x][p.y].y, ps, ps)
+    }
 }
 
 generateGrid();
