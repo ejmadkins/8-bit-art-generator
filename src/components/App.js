0e3ce92893d3943d9f74a9e0d3ba3c2635e8b5bc
Ed Adkins <ejmadkins@gmail.com>
2022-11-09 14:27:02 +0000

    style: add foundational components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import ColourPalette from './colourPallete/ColourPalette';
+import Canvas from './drawingCanvas/Canvas';
+import Menu from './menu/Menu';
+import SavedDrawings from './savedDrawings/SavedDrawings';
 
 function App() {
-  return <div className="App">8-bit-art-generator</div>;
+  return (
+    <div className="flex flex-wrap">
+      <div className="">
+        <ColourPalette></ColourPalette>
+      </div>
+      <div className="">
+        <Canvas></Canvas>
+        <Menu></Menu>
+        <SavedDrawings></SavedDrawings>
+      </div>
+    </div>
+  );
 }
 
 export default App;
